4b88565549132080e9054c72f51edf98cd727369
francis-edejer <francisedejer@yahoo.com>
2015-08-05 13:58:37 +0800

    Update pause_pingdom.rb

--- aus/recipes/pause_pingdom.rb
+++ aus/recipes/pause_pingdom.rb
@@ -4,10 +4,10 @@ ruby_block "pause_pingdom" do
     require 'net/https'
     require 'json'
 
-   PINGDOM_API_URL = 'https://api.pingdom.com'
-   PINGDOM_API_KEY = '1ihtcw3cxpue8y9ao1n3fsagi3tmkc4q'
-   PINGDOM_API_USERNAME = 'francis@pocketmath.com'
-   PINGDOM_API_PASSWORD = 'pmqrwe1423'
+    PINGDOM_API_URL = 'https://api.pingdom.com'
+    PINGDOM_API_KEY = '1ihtcw3cxpue8y9ao1n3fsagi3tmkc4q'
+    PINGDOM_API_USERNAME = 'francis@pocketmath.com'
+    PINGDOM_API_PASSWORD = 'pmqrwe1423'
 
     def get_pingdom(append_url)
       begin
